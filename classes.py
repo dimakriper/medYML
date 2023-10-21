9dcf4d7d29fb9f36ec20a1d185cdf9e0fdd352dc
dimakriper <dima.kutuzov@gmail.com>
2023-10-21 07:31:42 +0300

    add picture url

--- classes.py
+++ classes.py
@@ -8,7 +8,7 @@ class Shop:
         self.company = 'ООО "МЕДИКОР"'
         self.url = 'https://mcmedikor.ru'
         self.email = 'support-doctors@doctors.sample.s3.yandex.net'
-        self.picture = 'https://avatars.mds.yandex.net/get-pdb/5679262/13d16a0c-27e9-4095-8f55-accdc2d7c8f0/s1200'
+        self.picture = 'https://rawcdn.githack.com/dimakriper/medYML/8a5a17dfdec61c87629819f6a0e6153af2878628/%D0%BB%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF%20%D0%A6%D0%9D%D0%9C.jpg'
         self.description = 'Каталог врачей'
         self.currency = {'id': 'RUR', 'rate': '1'}
         self.categories = [{'id' : "1", 'content' : "Врач"}]
